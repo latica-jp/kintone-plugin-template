7061a4800aaf68d0ff0101c256453996a86cada6
Isao Yoshikoshi <latica.jp@gmail.com>
2019-04-08 07:35:32 +0900

    update: npm scripts

--- scripts/npm-start.js
+++ scripts/npm-start.js
@@ -1,15 +1,23 @@
-"use strict";
-const runAll = require("npm-run-all");
+'use strict';
+const runAll = require('npm-run-all');
 
-runAll(["develop", "upload"], {
+runAll(['develop -- --watch', 'upload -- --watch'], {
+  // upload を develop のあとではなく parallel で起動する理由：
+  // upload は watch モードでプラグインファイルの（再）生成を監視するため、
+  // 少なくとも develop の終了前に開始されている必要がある
   parallel: true,
   stdout: process.stdout,
-  stdin: process.stdin
-}).catch(({results}) => {
-  results
-    .filter(({code}) => code)
-    .forEach(({name}) => {
-      console.log(`"npm run ${name}" was failed`);
-    })
-  ;
+  stdin: process.stdin,
+}).catch(error => {
+  const { results } = error;
+  if (results) {
+    results
+      .filter(({ code }) => code)
+      .forEach(({ name }) => {
+        console.error(`"npm run ${name}" failed!`);
+      });
+  } else {
+    console.error(error);
+  }
+  process.exit(1);
 });
