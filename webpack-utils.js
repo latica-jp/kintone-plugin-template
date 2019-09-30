dc6d4c82e4f649b2f99ebcc6dcbe6e51dd055514
Isao Yoshikoshi <latica.jp@gmail.com>
2019-10-01 08:35:14 +0900

    fix: semver 対応を削除

--- webpack-utils.js
+++ webpack-utils.js
@@ -1,18 +1,10 @@
-const pluginVerToSemVer = pluginVer => {
-  const version = (parseFloat(pluginVer) / 100).toFixed(2)
-  const matches = version.match(/(\d+)\.(\d)(\d?)/)
-  if (!matches) return null
-  return `${matches[1]}.${matches[2]}.${matches[3] ? matches[3] : '0'}`
-}
-
 const createPluginZipFileName = () => {
   const config = require('./src/manifest.json')
   const desc = config.name.en
     .toLowerCase()
     .replace(/(\s|_)/g, '-')
     .replace(/[^a-z0-9-]+/g, '')
-  const semver = pluginVerToSemVer(config.version)
-  return `${desc}-${semver}.zip`
+  return `${desc}-${config.version}.zip`
 }
 
 const pluginZipFileName = createPluginZipFileName()
@@ -20,5 +12,4 @@ const pluginZipFileName = createPluginZipFileName()
 module.exports = {
   pluginZipFileName,
   pluginZipFilePath: `./dist/${pluginZipFileName}`,
-  pluginVerToSemVer,
 }
